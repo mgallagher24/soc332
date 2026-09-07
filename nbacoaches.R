4873beeed26529ce6be928b78c09c980933013a4
mgallagher24 <mrg97@duke.edu>
2026-09-07 01:54:51 -0400

    got all coach names in

---
+++ nbacoaches.R
@@ -0,0 +1,24 @@
+# load-packages ----
+library(tidyverse)
+library(rvest)
+library(janitor)
+
+
+# read page
+
+nbacoaches <- "https://www.basketball-reference.com/coaches/NBA_stats.html"
+nbapage<- read_html("https://www.basketball-reference.com/coaches/NBA_stats.html")
+
+nbacoachnames<- nbapage |>
+  html_elements("#coaches") |>
+  html_table()
+
+
+nbacoachnames <- as.data.frame(nbacoachnames) |>
+  select("Var.2")
+
+nbacoachnames <- clean_names(nbacoachnames)
+
+nbacoachnames <- nbacoachnames |>
+  filter(var_2 != "Coach") |>
+  filter(var_2 != "")

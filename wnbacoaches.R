4faeb0dec52cf96fe3d2e58c754a29255bcc7be7
mgallagher24 <mrg97@duke.edu>
2026-09-07 02:08:05 -0400

    added wnba coaches

---
+++ wnbacoaches.R
@@ -0,0 +1,24 @@
+
+
+# load-packages ----
+library(tidyverse)
+library(rvest)
+library(janitor)
+
+
+# read page
+wnbacoaches <- "https://www.basketball-reference.com/wnba/years/2026_coaches.html"
+wnbacoachespage <- read_html(wnbacoaches)
+
+wnbacoachnames<- wnbacoachespage |>
+  html_elements("#coaches") |>
+  html_table()
+
+wnbacoachnames <- as.data.frame(wnbacoachnames) |>
+  select("Var.1")
+
+wnbacoachnames <- clean_names(wnbacoachnames)
+
+wnbacoachnames <- wnbacoachnames |>
+  filter(var_1 != "Coach") |>
+  filter(var_1 != "")

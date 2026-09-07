ffa63837d94875a3c2950c4ef3b35019d82d6655
mgallagher24 <mrg97@duke.edu>
2026-09-07 02:03:15 -0400

    added mbb coaches

---
+++ mbbcoaches.R
@@ -0,0 +1,22 @@
+# load-packages ----
+library(tidyverse)
+library(rvest)
+library(janitor)
+
+
+# read page
+mbbcoaches <- "https://www.sports-reference.com/cbb/seasons/men/2026-coaches.html"
+mbbcoachespage <- read_html(mbbcoaches)
+
+mbbcoachnames<- mbbcoachespage |>
+  html_elements("#coaches") |>
+  html_table()
+
+mbbcoachnames <- as.data.frame(mbbcoachnames) |>
+  select("Var.1")
+
+mbbcoachnames <- clean_names(mbbcoachnames)
+
+mbbcoachnames <- mbbcoachnames |>
+  filter(var_1 != "Coach") |>
+  filter(var_1 != "")

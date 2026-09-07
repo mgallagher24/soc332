d2045d4d94868ee3174ade723dfe763cd4356c9b
mgallagher24 <mrg97@duke.edu>
2026-09-07 02:04:56 -0400

    added wbb coaches

---
+++ wbbcoaches.R
@@ -0,0 +1,22 @@
+# load-packages ----
+library(tidyverse)
+library(rvest)
+library(janitor)
+
+
+# read page
+wbbcoaches <- "https://www.sports-reference.com/cbb/seasons/women/2026-coaches.html"
+wbbcoachespage <- read_html(wbbcoaches)
+
+wbbcoachnames<- wbbcoachespage |>
+  html_elements("#coaches") |>
+  html_table()
+
+wbbcoachnames <- as.data.frame(wbbcoachnames) |>
+  select("Var.1")
+
+wbbcoachnames <- clean_names(wbbcoachnames)
+
+wbbcoachnames <- wbbcoachnames |>
+  filter(var_1 != "Coach") |>
+  filter(var_1 != "")

6d34ce379159b4d3d05678c85d7d5735cf99259f
mgallagher24 <mrg97@duke.edu>
2026-09-09 12:57:33 -0400

    added wnba coaches and assistant coaches (w/gender)

--- wnbacoaches.R
+++ wnbacoaches.R
@@ -29,8 +29,12 @@ wnbacoachnames <- wnbacoachnames |>
   mutate(
     gender = case_when(
       coach %in% c(
-        "Karl Smesko", "Tyler Marsh", "Rachid Meziane", "Jose Fernandez"
-        ) ~ "M")
+        "Karl Smesko", "Tyler Marsh", "Rachid Meziane", "Jose Fernandez",
+        "Chris DeMarco", "Nate Tibbetts", "Alex Sarama", "Sydney Johnson"
+        ) ~ "M",
+      coach %in% c("Natalie Nakase", "Stephanie White", "Lynne Roberts",
+                   "Becky Hammon", "Cheryl Reeve", "Sofia Raman",
+                   "Sandy Brondello", "Sonia Raman") ~ "F")
   )
 
 # assistant coaches ----
@@ -63,4 +67,94 @@ dal_asstsnames <- tibble(
 )  |>
   mutate(
     gender = if_else(coach %in% c("David Adkins", "Mike Neighbors"), "M", "F")
+  )
+gsv_asstsnames <- tibble(
+  coach = c("Sugar Rodgers", "Kasib Powell", "Landon Tatum"),
+  team = "GSV"
+)|>
+  mutate(
+    gender = if_else(coach == "Sugar Rodgers", "F", "M")
+  )
+ind_asstsnames <- tibble(
+  coach = c("Briann January", "Karima Christmas-Kelly", "Austin Kelly"),
+  team = "IND"
+)|>
+  mutate(
+    gender = if_else(coach == "Austin Kelly", "M", "F")
+  )
+las_asstsnames <- tibble(
+  coach = c("Ebony Hoffman", "Zach O’Brien", "Zak Buncik"),
+  team = "LAS"
+)|>
+  mutate(
+    gender = if_else(coach == "Ebony Hoffman", "F", "M")
+  )
+lva_asstsnames <- tibble(
+  coach = c("Nola Henry", "Larry Lewis", "Ty Ellis", "Charlene Thomas-Swinson",
+            "Micah Fraction"),
+  team = "LVA"
+) |>
+  mutate(
+    gender = if_else(coach %in% c("Ty Ellis", "Larry Lewis", "Micah Fraction"),
+                     "M", "F")
+  )
+min_asstsnames <- tibble(
+  coach = c("Janel McCarville", "Eric Thibault", "Lindsay Whalen",
+            "Rebekkah Brunson"),
+  team = "MIN"
+)|>
+  mutate(
+    gender = if_else(coach == "Eric Thibault", "M", "F")
+  )
+nyl_asstsnames <- tibble(
+  coach = c("Courtney Paris", "Addi Walters", "Will Sheehey",
+            "Andrew Wade"),
+  team = "NYL"
+)|>
+  mutate(
+    gender = if_else(coach %in% c("Andrew Wade", "Will Sheehey"),
+                     "M", "F")
+  )
+pho_asstsnames <- tibble(
+  coach = c("Kristi Toliver", "Megan Vogel", "TC Swirsky",
+            "John McCullough", "Chevelle Saunsoci", "Tangela Smith"),
+  team = "PHO"
+)|>
+  mutate(
+    gender = if_else(coach %in% c("TC Swirsky", "John McCullough"),
+                     "M", "F")
+  )
+por_asstsnames <- tibble(
+  coach = c("Danielle Boiago", "Sefu Bernard", "Sylvia Fowles",
+            "Brittni Donaldson"),
+  team = "POR"
+)|>
+  mutate(
+    gender = if_else(coach == "Sefu Bernard",
+                     "M", "F")
+  )
+sea_asstsnames <- tibble(
+  coach = c("Natalie Achonwa", "Jarell Christian", "Michael Joiner"),
+  team = "SEA"
+)|>
+  mutate(
+    gender = if_else(coach == "Natalie Achonwa",
+                     "F", "M")
+  )
+tor_asstsnames <- tibble(
+  coach = c("Ciara Carl", "Brian Lankton", "Carly Clarke",
+            "Sadie Edwards", "Olaf Lange"),
+  team = "TOR"
+)|>
+  mutate(
+    gender = if_else(coach %in% c("Brian Lankton", "Olaf Lange"),
+                     "M", "F")
+  )
+was_asstsnames <- tibble(
+  coach = c("Barbara Turner", "Jessie Miller", "Emre Vatansever"),
+  team = "WAS"
+)|>
+  mutate(
+    gender = if_else(coach == "Emre Vatansever",
+                     "M", "F")
   )

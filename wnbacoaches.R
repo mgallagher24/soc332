3db401782a2226337d435c00b12e88f14d9a5c13
mgallagher24 <mrg97@duke.edu>
2026-09-08 22:12:35 -0400

    added assistant coaches and coach genders for ATL, CHI, CON, and DAL

--- wnbacoaches.R
+++ wnbacoaches.R
@@ -24,3 +24,43 @@ wnbacoachnames <- wnbacoachnames |>
   filter(var_1 != "") |>
   rename("coach" = "var_1") |>
   rename("team" = "var_2")
+
+wnbacoachnames <- wnbacoachnames |>
+  mutate(
+    gender = case_when(
+      coach %in% c(
+        "Karl Smesko", "Tyler Marsh", "Rachid Meziane", "Jose Fernandez"
+        ) ~ "M")
+  )
+
+# assistant coaches ----
+
+wnba_asstsnames <- tibble(
+  coach = c("LaToya Sanders", "Brandi Poole", "Chelsea Lyles",
+  "Camryn Brown"),
+  team = "ATL",
+  gender = "F"
+)
+chi_asstsnames <- tibble(
+  coach = c("Jhared Simpson", "Latricia Trammell", 
+            "Rena Wakama", "Kelly Faris"),
+  team = "CHI"
+) |>
+  mutate(
+    gender = if_else(coach == "Jhared Simpson", "M", "F")
+  )
+con_asstsnames <- tibble(
+  coach = c("Roneeka Hodges", "Ashlee McGee", "Pascal Angillis"),
+  team = "CON"
+) |>
+  mutate(
+    gender = if_else(coach == "Pascal Angillis", "M", "F")
+  )
+dal_asstsnames <- tibble(
+  coach = c("Empress Davenport", "David Adkins", "Camille Smith",
+            "Mike Neighbors"),
+  team = "DAL"
+)  |>
+  mutate(
+    gender = if_else(coach %in% c("David Adkins", "Mike Neighbors"), "M", "F")
+  )
